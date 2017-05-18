123d8d2b1b7571bad13776877e0075bafa7798e5
Tarcio Nieri <tv.nieri@gmail.com>
2017-05-18 14:37:49 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Knowledge Pool
+
+### An application made for you to share Knowledge with your friends and teams.
---
+++ tcc/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/tvn/projects/knowledge_pool/tcc/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
